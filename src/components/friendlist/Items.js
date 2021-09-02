c4cdf3cb9385e3d5ddeb193b54a6e146d3b61b02
HETU PATEL <hetupatel.patidar@gmail.com>
2021-09-02 17:50:10 +0530

    Pagination added

--- src/components/friendlist/Items.js
+++ src/components/friendlist/Items.js
@@ -1,23 +1,33 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import ActionButtons from './ActionButtons';
 
 import { FriendContext } from '../../context/FriendListContext';
+import Pagination from './Pagination';
 
+let PageSize = 4;
 const Items = ({ name }) => {
    const { friendListData } = useContext(FriendContext);
    const [displayData, setDisplayData] = useState([]);
+   const [currentPage, setCurrentPage] = useState(0);
+
+   const currentTableData = useMemo(() => {
+      const firstPageIndex = (currentPage - 1) * PageSize;
+      const lastPageIndex = firstPageIndex + PageSize;
+      return displayData.slice(firstPageIndex, lastPageIndex);
+   }, [currentPage, displayData]);
 
    useEffect(() => {
       const searchedData = friendListData.filter((ele) =>
          ele.name.toLowerCase().startsWith(name.toLowerCase())
       );
       setDisplayData(searchedData);
+      setCurrentPage(1);
    }, [name, friendListData]);
 
    return (
       <div className="friendListItems">
          <ul>
-            {displayData.map((data) => (
+            {currentTableData.map((data) => (
                <li key={data.id}>
                   <div className="friendListItemsContent">
                      <strong>{data.name}</strong>
@@ -28,6 +38,13 @@ const Items = ({ name }) => {
                </li>
             ))}
          </ul>
+         <Pagination
+            className="pagination-bar"
+            currentPage={currentPage}
+            totalCount={displayData.length}
+            pageSize={PageSize}
+            onPageChange={(page) => setCurrentPage(page)}
+         />
       </div>
    );
 };
